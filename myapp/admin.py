5c44fadc389a7daf97ed06553bb865bd571c165e
Santiago Hernandez <sehernandezc@sanmateo.edu.co>
2024-11-05 19:57:51 -0500

    añadiendo estilos nuevos a las tarjetas y diseño general

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Vehiculo
+from .models import Vehiculo, Consumo
 # Register your models here.
 admin.site.register(Vehiculo)
+admin.site.register(Consumo)
